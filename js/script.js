940a7c3fa2898cb95d75f97d24f142e096dd009c
ksyu-gl <vatrushkasmolokom@gmail.com>
2018-12-09 12:43:20 +0300

    Изменила слайдер на баннере на бутстраповский

--- js/script.js
+++ js/script.js
@@ -1,19 +1,3 @@
-// PARALLAX
-
-$(window).scroll(function () {
-  parallax();
-});
-
-function parallax() {
-  var scrolled = $(window).scrollTop();
-  $('.main-bg').css('top', -(scrolled * 0.1) + 'px');
-//   $('.banner').css('top', 50 + (scrolled * 0.1) + '%');
-  $('.banner').css('opacity', 1 - (scrolled * 0.01) / 10);
-  $('.header').css('top', 50 + (scrolled * 0.1) + '%');
-  $('.header').css('opacity', 1 - (scrolled * 0.01) / 10);
-}
-
-
 // YANDEX
 
 ymaps.ready(init);
@@ -68,17 +52,4 @@ function init () {
 
 
 
-// SLIDER
 
-$('.review__block').slick({
-    dots: false,
-    arrows: false,
-    infinite: true,
-    speed: 300,
-    adaptiveHeight: true,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 500, 
-  });
- 
